f3c890df049e5096715bee5d891ec84be63ed82e
FelixvL <digitaleservice@gmail.com>
2022-08-24 11:41:32 +0200

    meegeven van gegevens via PathVariable

--- voordoen/src/main/java/endpoints/olc/voordoen/persistence/HuisService.java
+++ voordoen/src/main/java/endpoints/olc/voordoen/persistence/HuisService.java
@@ -3,6 +3,8 @@ package endpoints.olc.voordoen.persistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import endpoints.olc.voordoen.domein.Huis;
+
 @Service
 public class HuisService {
 	@Autowired
@@ -11,6 +13,16 @@ public class HuisService {
 	
 	public void methode1(){
 		System.out.println("ik ben in methode 1");
+		Huis huis1 = new Huis();
+		huis1.setEigenaar("Frits");
+		hr.save(huis1);
+	}
+
+
+	public void methode2(String owner) {
+		Huis huis1 = new Huis();
+		huis1.setEigenaar(owner);
+		hr.save(huis1);		
 	}
 
 }
--- voordoen/src/main/java/endpoints/olc/voordoen/rest/HuisEndpoint.java
+++ voordoen/src/main/java/endpoints/olc/voordoen/rest/HuisEndpoint.java
@@ -1,13 +1,30 @@
 package endpoints.olc.voordoen.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import endpoints.olc.voordoen.persistence.HuisService;
 
 @RestController
 public class HuisEndpoint {
+	@Autowired
+	HuisService hs;
 
 	@GetMapping("/methode1")
 	public void methode1() {
 		System.out.println("het endpoint is aangeroepen");
+		hs.methode1();
+	}
+	@GetMapping("/methode2/{eigenaar}")
+	public void methode2(@PathVariable("eigenaar") String owner) {
+		System.out.println("het endpoint is aangeroepen"+owner);
+		hs.methode2(owner);
+	}
+	@GetMapping("/methode3/{eigenaar}/{huisnummer}")
+	public void methode3(@PathVariable("eigenaar") String owner, @PathVariable("huisnummer") int hn) {
+		System.out.println("het endpoint is aangeroepen"+owner);
+		hs.methode2(owner);
 	}
 }
